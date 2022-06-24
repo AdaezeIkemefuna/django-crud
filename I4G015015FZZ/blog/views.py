f7eb1f5f3e0611b9159c6587c6ac2f0e2218a684
Ada-ikemefuna <adaezeikemefuna@yahoo.com>
2022-06-24 07:15:38 +0100

    modified views.py

--- I4G015015FZZ/blog/views.py
+++ I4G015015FZZ/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
 # Create your views here.
